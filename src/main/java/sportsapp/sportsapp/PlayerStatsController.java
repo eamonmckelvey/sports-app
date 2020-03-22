20d2cf0d9ab68bfc851be9b2f53a24e345697bc3
Muhammad Khokhar <muhdkhokhar@gmail.com>
2020-03-22 23:02:03 +0000

    * fixed player status issue.

--- src/main/java/sportsapp/sportsapp/PlayerStatsController.java
+++ src/main/java/sportsapp/sportsapp/PlayerStatsController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.support.SessionStatus;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
 
 
 @Slf4j
@@ -16,62 +18,72 @@ import javax.validation.Valid;
 @SessionAttributes("PlayerStats")
 public class PlayerStatsController {
 
-    private PlayerStatsRepository playerStatsRepository;
+	private PlayerStatsRepository playerStatsRepository;
 
-    public PlayerStatsController(PlayerStatsRepository playerStatsRepository) {this.playerStatsRepository = playerStatsRepository;}
+	@Autowired
+	private PlayerRepository playerRepository;
 
-    @Autowired
-    PlayerStatsRepository service;
+	public PlayerStatsController(PlayerStatsRepository playerStatsRepository) {this.playerStatsRepository = playerStatsRepository;}
 
-
-
-    @GetMapping("/addPlayerStats/{id}")
-    public String showSignUpFormPlayer1(Player player) {
-        return "addPlayerStats";
-    }
-
-    @PostMapping("/addPlayerStats/{id}")
-    public String processPlayer1(@Valid PlayerStats playerStats, BindingResult result, SessionStatus sessionStatus,
-                                 Player player, Fixtures fixtures, Model model) {
-        if (result.hasErrors()) {
-            return "player";
-        }
-
-        playerStats.setPlayer(player);
-        service.save(playerStats);
-        model.addAttribute("playerStats", service.findAll());
-        return "redirect:/player";
-    }
+	@Autowired
+	PlayerStatsRepository service;
 
 
-    @GetMapping("/editPlayerStats/{id}")
-    public String showUpdateFixturesForm (@PathVariable("id") long id, Model model){
-        PlayerStats playerStats = service.findAllById(id);
-        model.addAttribute("playerStats", playerStats);
-        return "editPlayerStats";
-    }
 
-    @PostMapping("/editPlayerStats/{id}")
-    public String processPlayer1d(@Valid PlayerStats playerStats, BindingResult result, SessionStatus sessionStatus,
-                                 Player player, Model model) {
-        if (result.hasErrors()) {
-            return "player";
-        }
+	@GetMapping("/addPlayerStats/{id}")
+	public String showSignUpFormPlayer1(Player player) {
+		return "addPlayerStats";
+	}
 
-        playerStats.setPlayer(player);
-        service.save(playerStats);
-        model.addAttribute("playerStats", service.findAll());
-        return "redirect:/player";
-    }
+	@PostMapping("/addPlayerStats/{id}")
+	public String processPlayer1(@Valid PlayerStats playerStats, BindingResult result, SessionStatus sessionStatus,
+								 Player player, Fixtures fixtures, Model model) {
+		if (result.hasErrors()) {
+			return "player";
+		}
 
-    //PlayerStats
+		playerStats.setPlayer(player);
+		playerStats.setId(null);
+		service.save(playerStats);
+		model.addAttribute("playerStats", service.findAll());
+		return "redirect:/player";
+	}
 
-    @GetMapping("/PlayerStats/{id}")
-    public String showUpdatePlayerForm3 (@PathVariable("id") long id, Model model){
-        PlayerStats playerStats = service.findAllById(id);
-        model.addAttribute("playerStats", playerStats);
-        return "playerStats";
-    }
+
+	@GetMapping("/editPlayerStats/{id}")
+	public String showUpdateFixturesForm (@PathVariable("id") long id, Model model){
+		PlayerStats playerStats = service.findAllById(id);
+		model.addAttribute("playerStats", playerStats);
+		return "editPlayerStats";
+	}
+
+	@PostMapping("/editPlayerStats/{id}")
+	public String processPlayer1d(@Valid PlayerStats playerStats, BindingResult result, SessionStatus sessionStatus,
+								  Player player, Model model) {
+		if (result.hasErrors()) {
+			return "player";
+		}
+
+		playerStats.setPlayer(player);
+		service.save(playerStats);
+		model.addAttribute("playerStats", service.findAll());
+		return "redirect:/player";
+	}
+
+	//PlayerStats
+
+	@GetMapping("/PlayerStats/{id}")
+	public String showUpdatePlayerForm3 (@PathVariable("id") long id, Model model){
+
+		//player id is being passed so we'll get the player first
+		Optional<Player> playerFound = playerRepository.findById(id);
+
+
+		//get the stats for this partciular player
+		List<PlayerStats> playerStats = service.findAllByPlayer(playerFound.get());
+		model.addAttribute("playerStats", playerStats);
+		return "playerStats";
+	}
 
 
 
--- src/main/java/sportsapp/sportsapp/PlayerStatsRepository.java
+++ src/main/java/sportsapp/sportsapp/PlayerStatsRepository.java
@@ -3,12 +3,16 @@ package sportsapp.sportsapp;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PlayerStatsRepository extends CrudRepository<PlayerStats, Long> {
 
-    PlayerStats findAllById(Long id);
+	PlayerStats findAllById(Long id);
 
-    PlayerStats findAllById(String id);
+	PlayerStats findAllById(String id);
+
+	List<PlayerStats> findAllByPlayer(final Player player);
 
 
 }
